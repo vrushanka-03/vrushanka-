9e4c294b0169928aedd586c1053ece8bd46c6408
vrushanka-03 <vrushankavd@gmail.com>
2024-12-30 14:07:54 +0530

    Create EscapeSequence.cpp

---
+++ EscapeSequence.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ 
+ int main() {
+
+    int i=0;
+
+    while(i<10) {
+
+ cout<<"hello my dearest adi\a";
+     i++;
+     
+     cout<<endl;
+
+    }
+
+    return 0;
+
+ }
